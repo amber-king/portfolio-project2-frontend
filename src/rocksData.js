8da38c99d9b375cdeffa074562db7f2218b52936
amber-king <amberking@pursuit.org>
2023-09-02 15:49:43 -0400

    add of mockdata for deploy workings

---
+++ src/rocksData.js
@@ -0,0 +1,66 @@
+// rocks mock data for deploy purposes on the frontend
+
+const rocksData = [
+  {
+    id: 1,
+    name: "Basalt",
+    element: "Igneous",
+    where_found: "active valcano",
+    color: "grey",
+    texture: "aphanitic",
+    luster: "dull",
+    hardness: true,
+  },
+  {
+    id: 2,
+    name: "Marble",
+    element: "Metamorphic",
+    where_found: "sedimentary limestone",
+    color: "multi-colored",
+    texture: "non-foliated",
+    luster: "not dull",
+    hardness: false,
+  },
+  {
+    id: 3,
+    name: "Sandstone",
+    element: "Sedimentary",
+    where_found: "sandy environment",
+    color: "multi-colored",
+    texture: "clastic",
+    luster: "dull",
+    hardness: true,
+  },
+  {
+    id: 4,
+    name: "Peridotite",
+    element: "Igneous",
+    where_found: "ocean floors",
+    color: "green",
+    texture: "coarse-grained",
+    luster: "not dull",
+    hardness: true,
+  },
+  {
+    id: 5,
+    name: "Gneiss",
+    element: "Metamorphic",
+    where_found: "mountains",
+    color: "multi-colored",
+    texture: "coarse-grained",
+    luster: "dull ",
+    hardness: true,
+  },
+  {
+    id: 6,
+    name: "Shale",
+    element: "Sedimentary",
+    where_found: "deltas",
+    color: "multi-color ",
+    texture: "smooth",
+    luster: "dull ",
+    hardness: true,
+  },
+];
+
+export default rocksData;
